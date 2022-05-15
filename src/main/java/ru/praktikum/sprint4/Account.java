2c0c46443dc0717233478018746f5115f5c6bf17
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-05-15 12:38:38 +0300

    debug info has been removed

--- src/main/java/ru/praktikum/sprint4/Account.java
+++ src/main/java/ru/praktikum/sprint4/Account.java
@@ -9,9 +9,7 @@ public class Account {
     }
 
     public boolean checkNameToEmboss() {
-        boolean result =  name.length() >= 3 && name.length() <= 19 && name.matches("^\\S+\\s\\S+$");
-        System.out.println(name + " : " + result);
-        return result;
+        return name.length() >= 3 && name.length() <= 19 && name.matches("^\\S+\\s\\S+$");
     }
 
 }
